9cd9e884d0250392b814c8c6f704b88da0b1d8cf
moljac <mcvjetko@holisticware.net>
2023-10-30 17:27:47 +0100

    new chatapp version provided by Javier for testing (navigation should work on Mac)

---
+++ chat-app-details/ChatApp/App.xaml.cs
@@ -0,0 +1,33 @@
+﻿using ChatApp.Models;
+using ChatApp.ViewModels;
+using ChatApp.Views;
+using System.Runtime.Intrinsics.X86;
+
+namespace ChatApp;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+        User user = new User
+        {
+            Name = "Alaya Cordova",
+            Image = "emoji1.png",
+            Color = Color.FromArgb("#FFE0EC")
+        };
+        var message = new Message
+        {
+            Sender = user,
+            Time = "18:32",
+            Text = "Hey there! What\'s up? Is everything ok?",
+        };
+
+        var vm = new DetailViewModel();
+        vm.InitializeAsync(message).Wait();
+        var detailView = new DetailView();
+        detailView.BindingContext = vm;
+        MainPage = new NavigationPage(detailView);
+	}
+}
