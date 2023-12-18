2571385737442d9d945281c368e69ff659f877f3
moljac <mcvjetko@holisticware.net>
2023-12-18 21:48:32 +0100

    tfms added

---
+++ chat-app-details/net8.0/ChatApp/App.xaml.cs
@@ -0,0 +1,42 @@
+﻿using ChatApp.Models;
+using ChatApp.ViewModels;
+using ChatApp.Views;
+using System.Runtime.Intrinsics.X86;
+
+namespace ChatApp;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+        User user = new User
+        {
+            Name = "Alaya Cordova",
+            Image = "emoji1.png",
+            Color = Color.FromArgb("#FFE0EC")
+        };
+        var message = new Message
+        {
+            Sender = user,
+            Time = "18:32",
+            Text = "Hey there! What\'s up? Is everything ok?",
+        };
+
+        /*
+        Directly to Details???
+
+        var vm = new DetailViewModel();
+        vm.InitializeAsync(message).Wait();
+        var detailView = new DetailView();
+        detailView.BindingContext = vm;
+        MainPage = new NavigationPage(detailView);
+        */
+
+        var vm = new HomeViewModel();
+        var v = new HomeView();
+        v.BindingContext = vm;
+        MainPage = new NavigationPage(v);
+	}
+}
